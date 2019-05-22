27d716430e0c986ad9e1817ec6b64ade1f6e8d2d
Burakhan <28931199+buralog@users.noreply.github.com>
2019-05-22 15:24:13 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,7 @@ const configureStore = () => {
   const store = createStore(
     rootReducer,
     compose(
-      applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      applyMiddleware(sagaMiddleware)
     )
   );
 
