d1fe4301993fed51d51da3ca026d60a563f72008
Burakhan <28931199+buralog@users.noreply.github.com>
2019-07-06 14:15:36 +0300

    Update gifsReducer.js

--- src/reducers/gifsReducer.js
+++ src/reducers/gifsReducer.js
@@ -3,8 +3,9 @@ import { GIFS } from '../constants';
 const gifsReducer = (state = {}, action) => {
     switch (action.type) {
         case GIFS.LOAD: {
-            console.log('gifsReducer state : ', state);
             return state.hasOwnProperty(action.searchTerm)
+            // when user searches one of the previous searchTerms again,
+            // load cached gifs from state
                 ? {
                       ...state,
                       [action.searchTerm]: [
@@ -12,6 +13,8 @@ const gifsReducer = (state = {}, action) => {
                           ...action.gifs,
                       ],
                   }
+            // when user searches a new term for the first time,
+            // load new results
                 : {
                       ...state,
                       [action.searchTerm]: [...action.gifs],
